73b790d893fd994b6d8f81e00a442faaab4b9d11
kongsheng.qyt <kongsheng.qyt@taobao.com>
2015-09-06 09:48:53 +0800

    add difference

--- cloud/app.js
+++ cloud/app.js
@@ -9,7 +9,7 @@ app.use(express.bodyParser());    // 读取请求 body 的中间件
 
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
 app.get('/hello', function(req, res) {
-  res.render('hello', { message: 'Congrats, you just set up your app!' });
+  res.render('hello', { message: 'Congrats, you just set up your app!^^' });
 });
 
 // 最后，必须有这行代码来使 express 响应 HTTP 请求
